64012f34225ab01e4943efaac687f6225745ebbd
Michael <michael@gmail.com>
2025-01-08 17:38:18 +0100

    tasks done

---
+++ labs/02-junit-tdd-calculator/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.addition(5, 5));
+        System.out.println(Calculator.subtraction(5, 5));
+        System.out.println(Calculator.multiplication(5, 5));
+    }
+}
--- labs/03-junit-tdd-oddeven/src/test/java/TestOddEvenChecker.java
+++ labs/03-junit-tdd-oddeven/src/test/java/TestOddEvenChecker.java
@@ -1,4 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestOddEvenChecker {
+    @Test
+    void testOddNumber() {
+        Assertions.assertTrue(OddEvenChecker.isOddNumber(25));
+        Assertions.assertTrue(OddEvenChecker.isOddNumber(15));
+    }
 
-
+    @Test
+    void testEvenNumber() {
+        Assertions.assertFalse(OddEvenChecker.isOddNumber(20));
+        Assertions.assertFalse(OddEvenChecker.isOddNumber(10));
+    }
 }
