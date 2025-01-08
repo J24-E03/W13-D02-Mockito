a5bbe649c4bace17aa6ae0af87775ff8c432dbc8
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2025-01-08 15:04:12 +0100

    Implement week 13 day 2 lab 1

--- labs/01-java-testing-mockito/src/test/java/com/dci/mockito/MathApplicationTest.java
+++ labs/01-java-testing-mockito/src/test/java/com/dci/mockito/MathApplicationTest.java
@@ -17,7 +17,6 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MathApplicationTest {
-
     @InjectMocks
     MathApplication mathApplication = new MathApplication();
 
@@ -27,31 +26,34 @@ public class MathApplicationTest {
     @Test
     @DisplayName("Use of When()  and thenReturn()")
     public void testMathApplicationUsingWhen() {
+        when(calcService.add(10.0, 20.0)).thenReturn(30.00);
+        when(calcService.subtract(20.0, 10.0)).thenReturn(10.00);
+        when(calcService.divide(20.0, 2.0)).thenReturn(10.00);
+        when(calcService.multiply(10.0, 20.0)).thenReturn(200.00);
 
-        //TODO: add the expectations of calc service calls
-
-        Assertions.assertEquals(mathApplication.add(10.0, 20.0), 30.0);
-        Assertions.assertEquals(mathApplication.subtract(20.0, 10.0), 10.0);
-        Assertions.assertEquals(mathApplication.divide(20.0, 2.0), 15.0);
-        Assertions.assertEquals(mathApplication.multiply(10.0, 20.0), 200.0);
+        Assertions.assertEquals(30.0, mathApplication.add(10.0, 20.0));
+        Assertions.assertEquals(10.0, mathApplication.subtract(20.0, 10.0));
+        Assertions.assertEquals(10.0, mathApplication.divide(20.0, 2.0));
+        Assertions.assertEquals(200.0, mathApplication.multiply(10.0, 20.0));
     }
 
     @Test
     @DisplayName("Verify method calls")
     public void testMathApplicationUsingVerify() {
-
         mathApplication.add(10.0, 20.0);
         mathApplication.subtract(20.0, 10.0);
         mathApplication.divide(20.0, 2.0);
         mathApplication.multiply(10.0, 20.0);
 
-        //TODO: verify calc service calls made for the above calls
+        verify(calcService).add(10.0, 20.0);
+        verify(calcService).subtract(20.0, 10.0);
+        verify(calcService).divide(20.0, 2.0);
+        verify(calcService).multiply(10.0, 20.0);
     }
 
     @Test
     @DisplayName("No' of method invocations/calls")
     public void testVerifyNoOfMethodCalls() {
-
         mathApplication.add(10.0, 20.0);
         mathApplication.add(10.0, 20.0);
         mathApplication.add(10.0, 20.0);
@@ -61,15 +63,14 @@ public class MathApplicationTest {
 
         mathApplication.divide(20.0, 2.0);
 
-        //TODO: verify calc service calls made for the above calls
-
-
+        verify(calcService, times(3)).add(10.0, 20.0);
+        verify(calcService, times(2)).subtract(20.0, 10.0);
+        verify(calcService, times(1)).divide(20.0, 2.0);
     }
 
     @Test
     @DisplayName("No' of method invocations/calls - 2")
     public void testVerifyNoOfMethodCalls2() {
-
         mathApplication.add(10.0, 20.0);
         mathApplication.add(10.0, 20.0);
         mathApplication.add(10.0, 20.0);
@@ -79,46 +80,46 @@ public class MathApplicationTest {
 
         mathApplication.divide(20.0, 2.0);
 
-        ////TODO: verify calc service calls made for the above calls
-
-
+        verify(calcService, atLeastOnce()).add(10.0, 20.0);
+        verify(calcService, atLeast(2)).subtract(20.0, 10.0);
+        verify(calcService, atMost(1)).divide(20.0, 2.0);
     }
 
     @Test
     @DisplayName("Exception Handling")
     public void verify_ExceptionHandling() {
-
-        //TODO:add the expected exceptions thrown for the below method calls
-
+        doThrow(new RuntimeException("Both Arguments can not be zero")).when(calcService).add(0.0, 0.0);
+        doThrow(new IllegalArgumentException("Invalid Arguments")).when(calcService).subtract(20.0, 20.0);
+        doThrow(new ArithmeticException("Can not be divided by zero")).when(calcService).divide(20.0, 0.0);
 
         RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> mathApplication.add(0, 0));
-        Assertions.assertEquals(runtimeException.getMessage(), "Both Arguments can not be zero");
+        Assertions.assertEquals("Both Arguments can not be zero", runtimeException.getMessage());
 
         IllegalArgumentException illegalArgumentException = Assertions.assertThrows(IllegalArgumentException.class, () -> mathApplication.subtract(20.0, 20.0));
-        Assertions.assertEquals(illegalArgumentException.getMessage(), "Invalid Arguments");
+        Assertions.assertEquals("Invalid Arguments", illegalArgumentException.getMessage());
 
         ArithmeticException arithmeticException = Assertions.assertThrows(ArithmeticException.class, () -> mathApplication.divide(20.0, 0.0));
-        Assertions.assertEquals(arithmeticException.getMessage(), "Can not be divided by zero");
+        Assertions.assertEquals("Can not be divided by zero", arithmeticException.getMessage());
     }
 
 
     @Test
     @DisplayName("Verify Method call sequence/order")
     public void verify_OrderOfMethodExecution() {
-
         when(calcService.add(10.0, 20.0)).thenReturn(30.00);
         when(calcService.subtract(20.0, 10.0)).thenReturn(10.00);
         when(calcService.divide(20.0, 2.0)).thenReturn(10.00);
         when(calcService.multiply(10.0, 20.0)).thenReturn(200.00);
 
-        Assertions.assertEquals(mathApplication.subtract(20.0, 10.0), 10.0);
-        Assertions.assertEquals(mathApplication.multiply(10.0, 20.0), 200.0);
-        Assertions.assertEquals(mathApplication.divide(20.0, 2.0), 10.0);
-        Assertions.assertEquals(mathApplication.add(10.0, 20.0), 30.0);
+        Assertions.assertEquals(10.0, mathApplication.subtract(20.0, 10.0));
+        Assertions.assertEquals(200.0, mathApplication.multiply(10.0, 20.0));
+        Assertions.assertEquals(10.0, mathApplication.divide(20.0, 2.0));
+        Assertions.assertEquals(30.0, mathApplication.add(10.0, 20.0));
 
-        //TODO:Verify Method call execution order
-
+        InOrder inOrder = inOrder(calcService);
+        inOrder.verify(calcService).subtract(20.0, 10.0);
+        inOrder.verify(calcService).multiply(10.0, 20.0);
+        inOrder.verify(calcService).divide(20.0, 2.0);
+        inOrder.verify(calcService).add(10.0, 20.0);
     }
-
-  
 }
